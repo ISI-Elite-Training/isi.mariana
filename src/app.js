cfd898d32a7df1ef3529684722c22165f20b343e
chvndler <70621588+chvndler@users.noreply.github.com>
2022-04-18 13:42:27 -0400

    PUSh

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 console.log('Hello world!');
+
+(function () {
+    // Set tenant name here
+    var TENANT_NAME = 'isi';
+
+    var d = document;
+    var sA = ['polyfills', 'js'];
+
+    for (var i = 0; i < sA.length; i++) {
+        var s = d.createElement('script');
+        s.src = 'https://' + TENANT_NAME + '.marianaiframes.com/' + sA[i];
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    }
+})();
