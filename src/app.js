7b0a4e07290ac9f47cec69dd555bfa74ad861b40
chvndler <70621588+chvndler@users.noreply.github.com>
2022-04-19 11:59:25 -0400

    buy page

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
-console.log('Hello world!');
-
 (function () {
+  var d = document;
+  var sA = ['polyfills', 'js'];
 
+  for (var i = 0; i < sA.length; i++) {
+    var s = d.createElement('script');
+    s.src = 'https://isi.marianaiframes.com/' + sA[i];
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+  }
 
-    var d = document;
-    var sA = ['polyfills', 'js'];
-
-    for (var i = 0; i < sA.length; i++) {
-        var s = d.createElement('script');
-        s.src = 'https://isi.marianaiframes.com/' + sA[i];
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    }
+  console.log('Mariana Script Loaded..');
 })();
